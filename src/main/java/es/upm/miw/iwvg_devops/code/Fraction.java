5c247fcf7f6c16990328cca83a88a2ed801a4479
Jano Martín <jano.martin.lopez@alumnos.upm.es>
2021-09-29 22:50:35 +0200

    fixed UsersDatabase #8

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -1,5 +1,17 @@
 package es.upm.miw.iwvg_devops.code;
 
 public class Fraction {
-    //ToDo: empty class for further development
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
 }
